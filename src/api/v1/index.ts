f4c25b020c8c255ea60ebfa4bc3bf694f313ba39
islam ezzat <islam3zzat@gmail.com>
2017-09-30 22:14:13 +0200

    adds cart and order endpoints

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import { authenticate } from '../../utils/auth';
+
 import authRouter from './auth';
+import cartRouter from './cart';
+import orderItemRouter from './orderItem';
+
 const v1Router = Router();
 v1Router.use('/auth', authRouter);
+v1Router.use('/cart', authenticate, cartRouter);
+v1Router.use('/order/:cartId', authenticate, orderItemRouter);
+
 export default v1Router;
