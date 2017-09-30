6547f27168e7c9216bd7241d02660e3b1bd249f2
islam ezzat <islam3zzat@gmail.com>
2017-09-30 22:12:01 +0200

    adds missing simicolon

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,10 +2,10 @@ import * as passport from 'passport';
 import * as expressJwt from 'express-jwt';
 export const authenticate = expressJwt({secret : 'shhhhhhhhh'});
 import { Response, Request, NextFunction } from 'express';
-import { sign } from './jwt'
+import { sign } from './jwt';
 
 
-export function authUser(strategy: string, req: Request, res: Response, next: NextFunction){
+export function authUser(strategy: string, req: Request, res: Response, next: NextFunction) {
     passport.authenticate(strategy, function(err: Error, user: {}, info: {}) {
         if (err) { return next(err); }
         if (!user) {
