7eafa00b21d2526141c463d22050d6d16c9c2f81
islam ezzat <islam3zzat@gmail.com>
2017-10-04 09:36:58 +0200

    enable cors temporarly

--- src/server.ts
+++ src/server.ts
@@ -52,7 +52,15 @@ mongoose.connection.on('error', () => {
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 3000);
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Authorization, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+app.set('port', process.env.PORT || 5000);
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 app.use(compression());
